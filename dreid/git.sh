b40f512d724120c60c77dbafdc550d20642d4bfe
David Reid <dreid@mochimedia.com>
2011-01-11 01:14:12 -0800

    Look harder for the git completion file.

--- dreid/git.sh
+++ dreid/git.sh
@@ -1,5 +1,14 @@
-GIT_COMP="/usr/local/etc/bash_completion.d/git-completion.bash";
-source $GIT_COMP
+GIT_COMP_SEARCH=(
+    "/usr/local/etc/bash_completion.d/git-completion.bash"
+    "/etc/bash_completion.d/git"
+)
+
+for git_comp in ${GIT_COMP_SEARCH[*]}; do
+    if [[ -f $git_comp ]]; then
+        source $git_comp
+        break
+    fi
+done
 
 GIT_PS1_SHOWDIRTY_STATE=t
 GIT_PS1_SHOWUPSTREAM="auto"
