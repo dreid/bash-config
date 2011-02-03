4704b208924ba2974b4fccb083c9b0536389917d
David Reid <dreid@mochimedia.com>
2011-02-02 21:05:13 -0800

    Some better hadoop stuff

--- dreid/mochi.sh
+++ dreid/mochi.sh
@@ -2,13 +2,14 @@ if [[ -d "/mochi" ]]; then
     M_VIRTUALENV=/mochi/opt/app-env/py27-ticket16883
     M_ERLANG_VER=R14B01
 
-    PATH=/mochi/opt/hadoop/bin:/mochi/opt/rabbitmq_server-2.1.1/sbin:$PATH
+    source /mochi/opt/hadoop/hadoop-env.sh
+
+    PATH=/mochi/opt/rabbitmq_server-2.1.1/sbin:$PATH
     PATH=/mochi/opt/erlang-${M_ERLANG_VER}/bin:$PATH
     PATH=/mochi/internal/bin:$PATH
-
-    HADOOP_HOME=/mochi/opt/hadoop/hadoop-0.20.2+737
+    PATH=$PATH:/mochi/bin
 
     alias mve='source ${M_VIRTUALENV}/bin/activate'
 
-    export HADOOP_HOME
+    export HADOOP_HOME OOZIE_URL
 fi
