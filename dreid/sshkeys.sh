9afe3f34e0cdd70bcdc1602a8fb85864c35ab6c8
David Reid <dreid@mochimedia.com>
2011-02-04 08:56:26 -0800

    More readable output

--- dreid/sshkeys.sh
+++ dreid/sshkeys.sh
@@ -1,7 +1,18 @@
 /usr/bin/ssh-add -l >& /dev/null
 
 if [[ $? != 0 ]]; then
+    echo
+    pad "-" 70
+    echo
     ssh-add
+    ssh_ret=$?
+    echo
+    pad "-" 70
+    echo
+    pad " " 61
+    if [[ $ssh_ret != 0 ]]; then
+        false
+    else
+        true
+    fi
 fi
-
-true
