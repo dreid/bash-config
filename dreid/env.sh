25b1ecb52e49f666a5171c40530ce2ec3ba2280d
David Reid <dreid@dreid.org>
2011-07-21 16:11:10 -0700

    node stuff for homebrew

--- dreid/env.sh
+++ dreid/env.sh
@@ -5,4 +5,6 @@ mkdir -p $WORKON_HOME
 PATH=~/bin:/usr/local/sbin:/usr/local/share/npm/bin:$PATH
 EDITOR=dtrt-emacs
 
-export PATH EDITOR WORKON_HOME
+NODE_PATH=/usr/local/lib/node
+
+export PATH EDITOR WORKON_HOME NODE_PATH
